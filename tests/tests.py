2a19bd6dd07e334a03c39340ad3cdb8ccd3ff5e4
Benjamin Kleeman <benjamin.kleeman@gmail.com>
2022-09-29 13:32:56 -0500

    gas wells tests

--- tests/tests.py
+++ tests/tests.py
@@ -1,9 +1,13 @@
 from unittest.result import failfast
 import requests
 
-MINES_ENDPOINT = 'http://127.0.0.1:5000/api/v0.1.0/infrastructure/2012/mines/coal'
-RAILROADS_ENDPOINT = 'http://127.0.0.1:5000/api/v0.1.0/infrastructure/2012/railroads'
+year = 2012
 
+MINES_ENDPOINT = f'http://127.0.0.1:5000/api/v0.1.0/infrastructure/{year}/mines/coal'
+RAILROADS_ENDPOINT = f'http://127.0.0.1:5000/api/v0.1.0/infrastructure/{year}/railroads'
+GAS_WELLS_ENDPOINT = f'http://127.0.0.1:5000/api/v0.1.0/infrastructure/{year}/wells/gas'
+
+# General tests, using coal mines
 def test_status_code():
     response = requests.get(MINES_ENDPOINT)
     assert response.status_code == 200
@@ -30,6 +34,8 @@ def test_has_coordinates():
         coords_exists = True
     assert True
 
+# Primary type exclusive tests, using railroads
+
 def test_primary_type_exclusive():
     response = requests.get(RAILROADS_ENDPOINT)
     response_body = response.json()
@@ -38,3 +44,19 @@ def test_primary_type_exclusive():
 def test_primary_type_exclusive_status_code():
     response = requests.get(RAILROADS_ENDPOINT)
     assert response.status_code == 200
+
+# Gas wells tests
+
+def test_gas_wells_status_code():
+    response = requests.get(GAS_WELLS_ENDPOINT)
+    assert response.status_code == 200
+
+def test_primary_type_gas_wells():
+    response = requests.get(GAS_WELLS_ENDPOINT)
+    response_body = response.json()
+    assert response_body['features'][0]['properties']['type']['primary'] == 'wells'
+
+def test_secondary_type_gas_wells():
+    response = requests.get(GAS_WELLS_ENDPOINT)
+    response_body = response.json()
+    assert response_body['features'][0]['properties']['type']['secondary'] == 'gas'
