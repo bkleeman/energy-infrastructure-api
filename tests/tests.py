20ee6422a59a4a36a50ae94d8502e1a59ac3c96e
Benjamin Kleeman <benjamin.kleeman@gmail.com>
2022-09-29 13:27:38 -0500

    update main url to be mines instead of general

--- tests/tests.py
+++ tests/tests.py
@@ -1,29 +1,29 @@
 from unittest.result import failfast
 import requests
 
-API_ENDPOINT_URL = 'http://127.0.0.1:5000/api/v0.1.0/infrastructure/2012/mines/coal'
+MINES_ENDPOINT = 'http://127.0.0.1:5000/api/v0.1.0/infrastructure/2012/mines/coal'
 RAILROADS_ENDPOINT = 'http://127.0.0.1:5000/api/v0.1.0/infrastructure/2012/railroads'
 
 def test_status_code():
-    response = requests.get(API_ENDPOINT_URL)
+    response = requests.get(MINES_ENDPOINT)
     assert response.status_code == 200
 
 def test_verify_json(): 
-    response = requests.get(API_ENDPOINT_URL)
+    response = requests.get(MINES_ENDPOINT)
     assert response.headers["Content-Type"] == 'application/json'
 
 def test_primary_type():
-    response = requests.get(API_ENDPOINT_URL)
+    response = requests.get(MINES_ENDPOINT)
     response_body = response.json()
     assert response_body['features'][0]['properties']['type']['primary'] == 'mines'
 
 def test_secondary_type():
-    response = requests.get(API_ENDPOINT_URL)
+    response = requests.get(MINES_ENDPOINT)
     response_body = response.json()
     assert response_body['features'][0]['properties']['type']['secondary'] == 'coal'
 
 def test_has_coordinates():
-    response = requests.get(API_ENDPOINT_URL)
+    response = requests.get(MINES_ENDPOINT)
     response_body = response.json()
     coords_exists = False
     if 'coordinates' in response_body['features'][0]['geometry']:
